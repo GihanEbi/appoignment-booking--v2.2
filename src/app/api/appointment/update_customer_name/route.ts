1ed25a8ee3a162703d712b05b22f91b0c3c8a047
Gihan Alwis <ebi011@jithpl.com>
2025-07-30 13:13:13 +0530

    update name in appointment

--- src/app/api/appointment/update_customer_name/route.ts
+++ src/app/api/appointment/update_customer_name/route.ts
@@ -3,9 +3,10 @@ import { NextResponse } from "next/server";
 // -------------services-----------------
 import { connectDB } from "../../../../../lib/db";
 import AppointmentModel from "../../../../../models/appointmentModel";
+import { appointment_constants } from "@/constants/appointment_constants";
 
 export async function POST(req: Request) {
-  const { appointmentId, customer_name } = await req.json();
+  const { phoneNo, customer_name } = await req.json();
 
   //   no token authentication
   // appointment will created by webhook in n8n
@@ -13,16 +14,16 @@ export async function POST(req: Request) {
   //   --------- connect to database -----------
   await connectDB();
 
-  // ------------ Check if appointmentId is provided -----------
-  if (!appointmentId || appointmentId.trim() === "") {
+  // ------------ Check if phone number is provided -----------
+  if (!phoneNo || phoneNo.trim() === "") {
     return NextResponse.json(
-      { success: false, message: "Appointment ID is required" },
+      { success: false, message: "Phone number is required" },
       { status: 400 },
     );
   }
 
   // ------------ Check if appointment exists -----------
-  const appointment = await AppointmentModel.find({ ID: appointmentId });
+  const appointment = await AppointmentModel.find({ phoneNo: phoneNo });
   if (!appointment || appointment.length === 0) {
     return NextResponse.json(
       { success: false, message: "Appointment not found" },
